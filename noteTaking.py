db4d0b18649804fcd02772a004d2404c4e0ab70e
erodbos <roddric.boswell@ericsson.com>
2021-03-20 14:18:13 -0500

    Added the writing to a file as a text and not as a list.  And added commits to the code.

--- noteTaking.py
+++ noteTaking.py
@@ -1,10 +1,24 @@
+"""
+This is a python script that that creates a simple note-taking program.  When a user starts it up
+it will prompt them for a filename.  If they enter a file name that doesn't exist, it will create
+it and prompt the user to start entering in the text they want to write to the file.
+
+If they enter a file name that already exists, it will ask the user if they want to:
+A) Read the file
+B) Delete the file and start over
+C) Append the file
+D) Replace a single line in the file.
+
+"""
 
 import os
 
+# Function that takes th input from the user and writes the data to a file.
 def takeNotes(x, tempValue):
     lines = []
     tempFile = open(inputFile, x)
     print("Please start entering your notes.  To exit the program please hit Enter twice")
+    print("\n")
     while True:
         line = input()
         if line:
@@ -19,9 +33,12 @@ def takeNotes(x, tempValue):
         tempFile.write(text)
     tempFile.close()
 
-inputFile = input("Please enter a file name: ")
+# Prompting user to enter a file name
+inputFile = input("\nPlease enter a file name: ")
+print("\n")
 tempValueList = ["A", "B", "C", "D"]
 
+# Checking to see if the file name already exist
 if os.path.exists(inputFile) == False:
     x = "w"
     dummyValue = ""
@@ -36,29 +53,30 @@ else:
     tempValue = input("Please enter A, B, C, or D: ")
     tempValue = tempValue.upper()
 
+# Verifying that the user enters one of the required values
     while not tempValue in tempValueList:
         tempValue = input("Please enter A, B, C, or D: ")
         tempValue = tempValue.upper()
     
     if tempValue == "A":
         tempFile = open(inputFile, "r")
+        print("\n")
         print(tempFile.read())
     
     if tempValue == "B":
-        print("The content in",inputFile,"will be deleted")
+        print("\nThe content in",inputFile,"will be deleted\n")
         os.remove(inputFile)
         x = "w"
         takeNotes(x,tempValue)
     
     if tempValue == "C":
-        print("Additional content will be added to",inputFile)
+        print("\nAdditional content will be added to",inputFile,"\n")
         x = "a"
         takeNotes(x, tempValue)
     
     if tempValue == "D":
         tempFile = open(inputFile, "r")
-        print("\n")
-        print("Current contents of the notes")
+        print("\nCurrent contents of the notes:\n")
         print(tempFile.read())
         tempFile.close()
         inputList = []
@@ -75,17 +93,23 @@ else:
         while i <= x:
             print("Which line do you wish to replace",i,inputList[i-1])
             i += 1
-        
+
+# Determining which line needs to be replace alone with the text that will replace it        
         print("\n")
         lineNumUpdate = int(input("Please enter the line number you wish to replace: "))
         textToReplace = input("Please enter the text you wish to update the line with:\n").split()
         lineNumUpdate -= 1
         inputList[lineNumUpdate] = textToReplace
-        
-        with open(inputFile, "w") as newNoteFile:
-            newNoteFile.write("\n".join(str(line) for line in inputList))
-            # for line in inputList:
-            #     newNoteFile.write("%s\n".join(str(line)))
+
+# Writing the new text to the inputFile
+        with open(inputFile, 'w') as newNoteFile:
+            for item in inputList:
+                newNoteFile.write(" ".join(map(str,item)))
+                newNoteFile.write("\n")
+
+
+
+
 
 
     
